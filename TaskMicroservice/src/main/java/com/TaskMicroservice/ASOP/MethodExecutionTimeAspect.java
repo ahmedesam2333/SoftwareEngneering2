114dd38b2ac4c5dca95dfe7b5b3cdb50994bc029
AhmedEssam <94251014+ahmedesam2333@users.noreply.github.com>
2024-05-02 12:57:05 +0300

    BackEnd

---
+++ TaskMicroservice/src/main/java/com/TaskMicroservice/ASOP/MethodExecutionTimeAspect.java
@@ -0,0 +1,17 @@
+package com.TaskMicroservice.ASOP;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+@Aspect
+@Component
+public class MethodExecutionTimeAspect {
+    @Around("execution(* com.TaskMicroservice.Controller.*.*(..))")
+    public Object measureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - startTime;
+        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return result;
+    }
+}
